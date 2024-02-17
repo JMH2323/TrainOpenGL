4bd4e3002704cda26449b73325afe2c2ffd9aed9
John Haley-Clay <128647462+JMH2323@users.noreply.github.com>
2024-02-17 12:37:11 -0500

    Update Main.cpp to show 3 triangles using index buffer and indices

--- Triangle/Main.cpp
+++ Triangle/Main.cpp
@@ -82,15 +82,27 @@ int main()
 	{
 		-0.5f, -0.7f * float(sqrt(3)) / 3, 0.0f, // Lower Left corner
 		0.5f, -0.7f * float(sqrt(3)) / 3, 0.0f, // Lower Right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner Left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+		0.0f, -1.4f * float(sqrt(3)) / 6, 0.0f // Inner down
 	};
 
+	GLuint indices[] =
+	{
+		0, 3, 5, // Lower left triangle
+		3, 2, 4, // Lower right triangle
+		5, 4, 1 // Upper triangle
+	};
+
+
 	// Create reference containers for Vertex Array Object and Vertex Buffer Object
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	// Generate the VAO and VBO with 1 object each
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// Make the VAO the current bound object
 	glBindVertexArray(VAO);
@@ -99,14 +111,18 @@ int main()
 	// Introduce the vertices into the VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// Configure attributes so OpenGL knows how to read VBO
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	// Enable attributes so OpenGL knows how to use them
 	glEnableVertexAttribArray(0);
 
-	// Bind both VBO and VAO to prevent modification
+	// Bind both VBO and VAO to prevent modification (EBO added)
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	
 
@@ -122,7 +138,7 @@ int main()
 		// Bind VAO so GL knows to use it
 		glBindVertexArray(VAO);
 		// Draw the Triangle using TRIANGLES Primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		// Swap Front-Back Buffers
 		glfwSwapBuffers(window);
 
@@ -134,6 +150,7 @@ int main()
 	// Clean Up
 	glDeleteVertexArrays(1, &VBO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
